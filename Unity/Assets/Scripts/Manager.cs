2a5ad268536de1f284c3ebe0bd06b5fa8edbf348
tony002009 <126559775+s316467@users.noreply.github.com>
2024-06-09 18:24:02 +0200

    feat: Add hardSlider and softSlider scripts

--- Unity/Assets/Scripts/Manager.cs
+++ Unity/Assets/Scripts/Manager.cs
@@ -2,95 +2,134 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Microsoft.MixedReality.Toolkit.UI;
 using TMPro;
+
 public class Manager : MonoBehaviour
 {
-    public GameObject Target, slider;
-    public TMP_Text visibilityButtonText;
-    public Material[] material; // vettore di materiali fra cui switchare
-    private new Renderer renderer;
-    private bool disabled = true;
-    private bool dist = true;
-    private bool land = true;
+    public GameObject SoftTarget, HardTarget, softSlider, hardSlider;
+    public TMP_Text softVisibilityButtonText, hardVisibilityButtonText;
+    public Material[] material; // Array of materials to switch between
+    private new Renderer softRenderer, hardRenderer;
+    private bool softDisabled = true;
+    private bool hardDisabled = true;
+    private bool softDist = true;
+    private bool hardDist = true;
+    private bool softLand = true;
+    private bool hardLand = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (Target == null) //Target not assigned through the inspector
+        if (SoftTarget == null) // SoftTarget not assigned through the inspector
         {
-            //Search Target GameObject with associated class
-            //Finding GameObject with associated scripts is the most robust and secure way
-            SimpleTarget simpleTarget = GameObject.FindObjectOfType<SimpleTarget>();
-            if (simpleTarget != null)
+            SimpleTargetSoft softSimpleTarget = GameObject.FindObjectOfType<SimpleTargetSoft>();
+            if (softSimpleTarget != null)
             {
-                Target = simpleTarget.gameObject;
+                SoftTarget = softSimpleTarget.gameObject;
             }
         }
-        renderer = Target.GetComponent<Renderer>();
-        renderer.enabled = true;
-        slider.SetActive(true);
+        if (HardTarget == null) // HardTarget not assigned through the inspector
+        {
+            SimpleTargetHard hardSimpleTarget = GameObject.FindObjectOfType<SimpleTargetHard>();
+            if (hardSimpleTarget != null)
+            {
+                HardTarget = hardSimpleTarget.gameObject;
+            }
+        }
+
+        if (SoftTarget != null)
+        {
+            softRenderer = SoftTarget.GetComponent<Renderer>();
+            softRenderer.enabled = true;
+        }
+
+        if (HardTarget != null)
+        {
+            hardRenderer = HardTarget.GetComponent<Renderer>();
+            hardRenderer.enabled = true;
+        }
+
+        softSlider.SetActive(true);
+        hardSlider.SetActive(true);
     }
+
     public void OnRestartTouch()
     {
-        SceneManager.LoadScene("02_Landmark"); //Load scene called "" from Start;
+        SceneManager.LoadScene("SampleScene"); // Load scene called "" from Start;
     }
-    public void Hide(PinchSlider slider)
+
+    public void HideSoft(PinchSlider slider)
     {
-        if (disabled)
+        if (softDisabled)
         {
-            //Call della funzione ChangeAlpha con input il valore di alpha=0 --> trasparenza totale
-            ChangeAlpha(0);
-            //aggiorno il valore dello slider
+            ChangeAlpha(0, softRenderer);
             slider.SliderValue = 0;
-            //aggiorno il testo del button
-            visibilityButtonText.text = "Show Model";
-            disabled = false;
+            softVisibilityButtonText.text = "Show Soft Model";
+            softDisabled = false;
         }
         else
         {
-            //Call della funzione ChangeAlpha con input il valore di alpha=1 --> opacità totale
-            ChangeAlpha(1);
-            //aggiorno il valore dello slider
+            ChangeAlpha(1, softRenderer);
             slider.SliderValue = 1;
-            //aggiorno il testo del button
-            visibilityButtonText.text = "Hide Model";
-            disabled = true;
+            softVisibilityButtonText.text = "Hide Soft Model";
+            softDisabled = true;
         }
     }
-    public void ChangeOpacity(SliderEventData eventData)
+
+    public void HideHard(PinchSlider slider)
     {
-        // Call della funzione ChangeAlpha con input il valore di alpha indicato sullo slider
-        ChangeAlpha(eventData.NewValue);
+        if (hardDisabled)
+        {
+            ChangeAlpha(0, hardRenderer);
+            slider.SliderValue = 0;
+            hardVisibilityButtonText.text = "Show Hard Model";
+            hardDisabled = false;
+        }
+        else
+        {
+            ChangeAlpha(1, hardRenderer);
+            slider.SliderValue = 1;
+            hardVisibilityButtonText.text = "Hide Hard Model";
+            hardDisabled = true;
+        }
     }
-    public void ChangeAlpha(float alphaVal)
+
+    public void ChangeSoftOpacity(SliderEventData eventData)
+    {
+        ChangeAlpha(eventData.NewValue, softRenderer);
+    }
+
+    public void ChangeHardOpacity(SliderEventData eventData)
+    {
+        ChangeAlpha(eventData.NewValue, hardRenderer);
+    }
+
+    public void ChangeAlpha(float alphaVal, Renderer renderer)
     {
         if (alphaVal == 1)
         {
-            // render del materiale opaco
-            renderer.sharedMaterial = material[1];
+            renderer.sharedMaterial = material[1]; // Opaque material
         }
         else
         {
-            // render del materiale fade
-            renderer.sharedMaterial = material[0];
-            // assegno alla variabile fadeColor di tipo Color il colore corrente del materiale fade
+            renderer.sharedMaterial = material[0]; // Fade material
             Color fadeColor = material[0].color;
-            // modifico il valore del canale alpha assegnando il valore di alpha in input
             fadeColor.a = alphaVal;
-            //setto il nuovo colore per il materiale fade
             material[0].SetColor("_Color", fadeColor);
         }
     }
-    public void Hidedistance()
+
+    public void HideSoftDistance()
     {
         LineRenderer[] linerend = GameObject.FindObjectsOfType<LineRenderer>();
         TargetText[] meshrend = GameObject.FindObjectsOfType<TargetText>();
-        if (dist)
+        if (softDist)
         {
             for (int i = 0; i < linerend.Length; i++)
             {
                 linerend[i].gameObject.GetComponent<LineRenderer>().enabled = false;
                 meshrend[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
             }
-            dist = false;
+            softDist = false;
         }
         else
         {
@@ -99,19 +138,44 @@ public class Manager : MonoBehaviour
                 linerend[i].gameObject.GetComponent<LineRenderer>().enabled = true;
                 meshrend[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
             }
-            dist = true;
+            softDist = true;
         }
     }
-    public void Hidelandmark()
+
+    public void HideHardDistance()
     {
-        TargetLandmark[] landTarget = GameObject.FindObjectsOfType<TargetLandmark>();
-        if (land)
+        LineRenderer[] linerend = GameObject.FindObjectsOfType<LineRenderer>();
+        TargetText[] meshrend = GameObject.FindObjectsOfType<TargetText>();
+        if (hardDist)
+        {
+            for (int i = 0; i < linerend.Length; i++)
+            {
+                linerend[i].gameObject.GetComponent<LineRenderer>().enabled = false;
+                meshrend[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            hardDist = false;
+        }
+        else
+        {
+            for (int i = 0; i < linerend.Length; i++)
+            {
+                linerend[i].gameObject.GetComponent<LineRenderer>().enabled = true;
+                meshrend[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            hardDist = true;
+        }
+    }
+
+    public void HideSoftLandmark()
+    {
+        TargetLandmarkSoft[] landTarget = GameObject.FindObjectsOfType<TargetLandmarkSoft>();
+        if (softLand)
         {
             for (int i = 0; i < landTarget.Length; i++)
             {
                 landTarget[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
             }
-            land = false;
+            softLand = false;
         }
         else
         {
@@ -119,7 +183,28 @@ public class Manager : MonoBehaviour
             {
                 landTarget[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
             }
-            land = true;
+            softLand = true;
+        }
+    }
+
+    public void HideHardLandmark()
+    {
+        TargetLandmarkHard[] landTarget = GameObject.FindObjectsOfType<TargetLandmarkHard>();
+        if (hardLand)
+        {
+            for (int i = 0; i < landTarget.Length; i++)
+            {
+                landTarget[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            hardLand = false;
+        }
+        else
+        {
+            for (int i = 0; i < landTarget.Length; i++)
+            {
+                landTarget[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            hardLand = true;
         }
     }
 }
